ddd13a7b3e3b1977e292720a4124703808d959c7
Juan Zacarias <juan457@gmail.com>
2012-05-07 15:10:24 -0500

    Created structure of zip module

---
+++ src/zip.xq.src/zip.cpp
@@ -0,0 +1,202 @@
+
+#include <zorba/user_exception.h>
+#include <zorba/item_factory.h>
+#include <zorba/singleton_item_sequence.h>
+
+#include "zip.h"
+
+namespace zorba { namespace zip {
+
+/*******************************************************************************************
+ *******************************************************************************************/
+
+  zorba::ExternalFunction*
+    ZipModule::getExternalFunction(const zorba::String& localName)
+  {
+    FuncMap_t::iterator lIte = theFunctions.find(localName);
+
+    ExternalFunction*& lFunc = theFunctions[localName];
+
+    if (lIte == theFunctions.end())
+    {
+      if (localName == "create")
+      {
+        lFunc = new CreateFunction(this);
+      }
+      if (localName == "entries")
+      {
+        lFunc = new EntriesFunction(this);
+      }
+      if (localName == "get-text")
+      {
+        lFunc = new GetTextFunction(this);
+      }
+      if (localName == "get-binary")
+      {
+        lFunc = new GetBinaryFunction(this);
+      }
+      if (localName == "add")
+      {
+        lFunc = new AddFunction(this);
+      }
+      if (localName == "replace")
+      {
+        lFunc = new ReplaceFunction(this);
+      }
+      if (localName == "delete")
+      {
+        lFunc = new DeleteFunction(this);
+      }
+    }
+
+    return lFunc;
+  }
+
+  void ZipModule::destroy()
+  {
+    delete this;
+  }
+
+  ZipModule::~ZipModule()
+  {
+    for (FuncMap_t::const_iterator lIter = theFunctions.begin();
+      lIter != theFunctions.end(); ++lIter)
+    {
+      delete lIter->second;
+    }
+    theFunctions.clear();
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+  
+  ZipFunction::ZipFunction(const ZipModule* aModule)
+    : theModule(aModule)
+  {
+  }
+
+  ZipFunction::~ZipFunction()
+  {
+  }
+
+  String 
+    ZipFunction::getURI() const
+  {
+    return theModule->getURI();
+  }
+
+  void
+    ZipFunction::throwError(const char *err_localname, const std::string aErrorMessage)
+  {
+    String errNS(ZIP_MODULE_NAMESPACE);
+    String errName(err_localname);
+    Item errQName = ZipModule::getItemFactory()->createQName(errNS, errName);
+    String errDescription(aErrorMessage);
+    throw USER_EXCEPTION(errQName, errDescription);
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    CreateFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    EntriesFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    GetTextFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    GetBinaryFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    UpdateFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    AddFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    ReplaceFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+/*******************************************************************************************
+ *******************************************************************************************/
+ 
+  zorba::ItemSequence_t
+    DeleteFunction::evaluate(
+      const Arguments_t& aArgs,
+      const zorba::StaticContext* aSctx,
+      const zorba::DynamicContext* aDctx) const 
+  {
+    return ItemSequence_t(new SingletonItemSequence(
+      ZipModule::getItemFactory()->createString("Function not implemented")));
+  }
+
+} /* namespace zorba */ } /* namespace zip*/
---
+++ src/zip.xq.src/zip.h
@@ -0,0 +1,194 @@
+#ifndef _ORG_EXPATH_WWW_NS_ZIP2_H_
+#define _ORG_EXPATH_WWW_NS_ZIP2_H_
+
+#include <map>
+
+#include <zorba/zorba.h>
+#include <zorba/external_module.h>
+#include <zorba/function.h>
+#include <zorba/dynamic_context.h>
+
+#define ZIP_MODULE_NAMESPACE "http://www.expath.org/ns/zip2"
+
+namespace zorba { namespace zip {
+
+  class ZipModule : public ExternalModule {
+    protected:
+      class ltstr
+      {
+      public:
+        bool operator()(const String& s1, const String& s2) const
+        {
+          return s1.compare(s2) < 0;
+        }
+      };
+
+      typedef std::map<String, ExternalFunction*, ltstr> FuncMap_t;
+
+      FuncMap_t theFunctions;
+
+    public:
+
+      virtual ~ZipModule();
+
+      virtual zorba::String
+        getURI() const { return ZIP_MODULE_NAMESPACE; }
+
+      virtual zorba::ExternalFunction*
+        getExternalFunction(const String& localName);
+
+      virtual void destroy();
+
+      static ItemFactory*
+        getItemFactory()
+      {
+        return Zorba::getInstance(0)->getItemFactory();
+      }
+
+  };
+
+  class ZipFunction : public ContextualExternalFunction
+  {
+    protected:
+      const ZipModule* theModule;
+
+      static void
+        throwError(const char*, const std::string);
+
+      XQuery_t
+        getQuery(
+          const zorba::DynamicContext* dctx,
+          const zorba::String& aIdent) const;
+    public:
+
+      ZipFunction(const ZipModule* module);
+
+      virtual ~ZipFunction();
+
+      virtual String
+        getURI() const;
+  };
+
+  class CreateFunction : public ZipFunction{
+    public:
+      CreateFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~CreateFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "create"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class EntriesFunction : public ZipFunction{
+    public:
+      EntriesFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~EntriesFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "entries"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class GetTextFunction : public ZipFunction{
+    public:
+      GetTextFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~GetTextFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "get-text"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class GetBinaryFunction : public ZipFunction{
+    public:
+      GetBinaryFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~GetBinaryFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "get-binary"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class UpdateFunction : public ZipFunction{
+    public:
+      UpdateFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~UpdateFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "update"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class AddFunction : public ZipFunction{
+    public:
+      AddFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~AddFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "add"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class ReplaceFunction : public ZipFunction{
+    public:
+      ReplaceFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~ReplaceFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "replace"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+  class DeleteFunction : public ZipFunction{
+    public:
+      DeleteFunction(const ZipModule* aModule) : ZipFunction(aModule) {}
+
+      virtual ~DeleteFunction(){}
+
+      virtual zorba::String
+        getLocalName() const { return "delete"; }
+
+      virtual zorba::ItemSequence_t
+        evaluate(const Arguments_t&,
+                 const zorba::StaticContext*,
+                 const zorba::DynamicContext*) const;
+  };
+
+} /* namespace zip  */ } /* namespace zorba */
+
+#endif // _ORG_EXPATH_WWW_NS_ZIP2_H_
